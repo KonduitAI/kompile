af9ac9d880804fff81a8bb8201a46edb31023e75
Adam Gibson <adam@konduit.ai>
2021-08-10 15:18:09 +0900

    Update setup.py

--- kompile-python/setup.py
+++ kompile-python/setup.py
@@ -1,33 +1,23 @@
+import sysconfig
+# me make damn sure, that disutils does not mess with our
+# build process
 
-from distutils.sysconfig import get_config_vars as default_get_config_vars
+sysconfig.get_config_vars()['CFLAGS'] = ''
+sysconfig.get_config_vars()['OPT'] = ''
+sysconfig.get_config_vars()['PY_CFLAGS'] = ''
+sysconfig.get_config_vars()['PY_CORE_CFLAGS'] = ''
+sysconfig.get_config_vars()['CC'] = 'gcc'
+sysconfig.get_config_vars()['CXX'] = 'g++'
+sysconfig.get_config_vars()['BASECFLAGS'] = ''
+sysconfig.get_config_vars()['CCSHARED'] = '-fPIC'
+sysconfig.get_config_vars()['LDSHARED'] = 'gcc -shared'
+sysconfig.get_config_vars()['CPP'] = ''
+sysconfig.get_config_vars()['CPPFLAGS'] = ''
+sysconfig.get_config_vars()['BLDSHARED'] = ''
+sysconfig.get_config_vars()['CONFIGURE_LDFLAGS'] = ''
+sysconfig.get_config_vars()['LDFLAGS'] = ''
+sysconfig.get_config_vars()['PY_LDFLAGS'] = ''
 
-
-def remove_extra(x,target_string):
-    if type(x) is str:
-        # x.replace(" -pthread ") would be probably enough...
-        # but we want to make sure we make it right for every input
-        if x==target_string:
-            return ""
-        if x.startswith(" %s ".format(target_string)):
-            return remove_extra(x[len("%s  ".format(target_string)):])
-        if x.endswith(" %s ".format(target_string)):
-            return remove_extra(x[:-len(" %s ".format(target_string))])
-        return x.replace(" %s  ".format(target_string), " ")
-    return x
-
-def my_get_config_vars(*args):
-  result = default_get_config_vars(*args)
-  # sometimes result is a list and sometimes a dict:
-  if type(result) is list:
-     return [remove_extra(remove_extra(x,'-n1'),'.2-a+fp16+rcpc+dotprod+crypto:') for x in result]
-  elif type(result) is dict:
-     return {k : remove_extra(remove_extra(x,'-n1'),'.2-a+fp16+rcpc+dotprod+crypto:') for k,x in result.items()}
-  else:
-     raise Exception("cannot handle type"+type(result))
-
-# 2.step: replace
-import distutils.sysconfig as dsc
-dsc.get_config_vars = my_get_config_vars
 
 from distutils.core import setup
 from distutils.extension import Extension
