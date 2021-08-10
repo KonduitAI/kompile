4a75c31f343f879d60b37a6bda7af085a4c7c061
Adam Gibson <adam@konduit.ai>
2021-08-10 15:01:38 +0900

    Update setup.py

--- kompile-python/setup.py
+++ kompile-python/setup.py
@@ -6,10 +6,11 @@ import numpy
 compiler_directives = {"language_level": 3, "embedsignature": True}
 ext_modules = cythonize(
     Extension('kompile.interface.native.interface',
+              extra_compile_args=['-mtune=generic'],
               sources=['kompile/interface/native/interface.pyx'],
               include_dirs=['./include',numpy.get_include()],
               library_dirs=['./lib'],
-              libraries=['kompile_c_library','konduit-serving'] ,
+              libraries=['kompile_c_library','konduit-serving'],
               language='c'
               ), compiler_directives=compiler_directives)
 
