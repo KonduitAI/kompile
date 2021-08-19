a46a8400d529022f948ac0fcc74cebf623d5299b
Adam Gibson <adam@konduit.ai>
2021-08-19 22:23:44 +0900

    Update setup.py

--- kompile-python/setup.py
+++ kompile-python/setup.py
@@ -1,22 +1,22 @@
-# from distutils import sysconfig
+from distutils import sysconfig
 # # me make damn sure, that disutils does not mess with our
 # # build process
 #
-# sysconfig.get_config_vars()['CFLAGS'] = ''
-# sysconfig.get_config_vars()['OPT'] = ''
-# sysconfig.get_config_vars()['PY_CFLAGS'] = ''
-# sysconfig.get_config_vars()['PY_CORE_CFLAGS'] = ''
-# sysconfig.get_config_vars()['CC'] = 'gcc'
-# sysconfig.get_config_vars()['CXX'] = 'g++'
-# sysconfig.get_config_vars()['BASECFLAGS'] = ''
-# sysconfig.get_config_vars()['CCSHARED'] = '-fPIC'
-# sysconfig.get_config_vars()['LDSHARED'] = 'gcc -shared'
-# sysconfig.get_config_vars()['CPP'] = ''
-# sysconfig.get_config_vars()['CPPFLAGS'] = ''
-# sysconfig.get_config_vars()['BLDSHARED'] = ''
-# sysconfig.get_config_vars()['CONFIGURE_LDFLAGS'] = ''
-# sysconfig.get_config_vars()['LDFLAGS'] = ''
-# sysconfig.get_config_vars()['PY_LDFLAGS'] = ''
+sysconfig.get_config_vars()['CFLAGS'] = ''
+sysconfig.get_config_vars()['OPT'] = ''
+sysconfig.get_config_vars()['PY_CFLAGS'] = ''
+sysconfig.get_config_vars()['PY_CORE_CFLAGS'] = ''
+sysconfig.get_config_vars()['CC'] = 'gcc'
+sysconfig.get_config_vars()['CXX'] = 'g++'
+sysconfig.get_config_vars()['BASECFLAGS'] = ''
+sysconfig.get_config_vars()['CCSHARED'] = '-fPIC'
+sysconfig.get_config_vars()['LDSHARED'] = 'gcc -shared'
+sysconfig.get_config_vars()['CPP'] = ''
+sysconfig.get_config_vars()['CPPFLAGS'] = ''
+sysconfig.get_config_vars()['BLDSHARED'] = ''
+sysconfig.get_config_vars()['CONFIGURE_LDFLAGS'] = ''
+sysconfig.get_config_vars()['LDFLAGS'] = ''
+sysconfig.get_config_vars()['PY_LDFLAGS'] = ''
 
 
 from setuptools.extension import Extension
@@ -27,11 +27,16 @@ import os
 compiler_directives = {"language_level": 3, "embedsignature": True}
 
 _LIB_OUTPUT_PATH = 'LIB_OUTPUT_PATH'
+lib_list = []
+if _LIB_OUTPUT_PATH in os.environ:
+    lib_list.append(os.environ[_LIB_OUTPUT_PATH])
+else:
+    raise Exception('Unable to build. Please specify a library output path with environment variable LIB_OUTPUT_PATH')
 extension = Extension('kompile.interface.native.interface',
               extra_compile_args=[],
               sources=['kompile/interface/native/interface.pyx'],
-              include_dirs=['./include',numpy.get_include()],
-              library_dirs=['./lib'],
+              include_dirs=[numpy.get_include()],
+              library_dirs=[lib_list],
               libraries=['kompile_c_library','konduit-serving'],
               language='c'
 )
