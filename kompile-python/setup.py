7d76a7bd7f15258c889c6e19801617e907414ebe
Adam Gibson <adam@konduit.ai>
2022-07-07 19:00:55 +0900

    Update setup.py

--- kompile-python/setup.py
+++ kompile-python/setup.py
@@ -1,23 +1,25 @@
-from distutils import sysconfig
+from distutils import sysconfig as dist_sysconfig
 import sys
 # # me make damn sure, that disutils does not mess with our
 # # build process
 #
-sysconfig.get_config_vars()['CFLAGS'] = ''
-sysconfig.get_config_vars()['OPT'] = ''
-sysconfig.get_config_vars()['PY_CFLAGS'] = ''
-sysconfig.get_config_vars()['PY_CORE_CFLAGS'] = ''
-sysconfig.get_config_vars()['CC'] = 'gcc'
-sysconfig.get_config_vars()['CXX'] = 'g++'
-sysconfig.get_config_vars()['BASECFLAGS'] = ''
-sysconfig.get_config_vars()['CCSHARED'] = '-fPIC'
-sysconfig.get_config_vars()['LDSHARED'] = 'gcc -shared'
-sysconfig.get_config_vars()['CPP'] = ''
-sysconfig.get_config_vars()['CPPFLAGS'] = ''
-sysconfig.get_config_vars()['BLDSHARED'] = ''
-sysconfig.get_config_vars()['CONFIGURE_LDFLAGS'] = ''
-sysconfig.get_config_vars()['LDFLAGS'] = ''
-sysconfig.get_config_vars()['PY_LDFLAGS'] = ''
+dist_sysconfig.get_config_vars()['CFLAGS'] = ''
+dist_sysconfig.get_config_vars()['OPT'] = ''
+dist_sysconfig.get_config_vars()['PY_CFLAGS'] = ''
+dist_sysconfig.get_config_vars()['PY_CORE_CFLAGS'] = ''
+dist_sysconfig.get_config_vars()['CC'] = 'gcc'
+dist_sysconfig.get_config_vars()['CXX'] = 'g++'
+dist_sysconfig.get_config_vars()['BASECFLAGS'] = ''
+dist_sysconfig.get_config_vars()['CCSHARED'] = '-fPIC'
+dist_sysconfig.get_config_vars()['LDSHARED'] = 'gcc -shared'
+dist_sysconfig.get_config_vars()['CPP'] = ''
+dist_sysconfig.get_config_vars()['CPPFLAGS'] = ''
+dist_sysconfig.get_config_vars()['BLDSHARED'] = ''
+dist_sysconfig.get_config_vars()['CONFIGURE_LDFLAGS'] = ''
+dist_sysconfig.get_config_vars()['LDFLAGS'] = ''
+dist_sysconfig.get_config_vars()['PY_LDFLAGS'] = ''
+
+
 
 
 from setuptools.extension import Extension
@@ -26,6 +28,8 @@ from Cython.Build import cythonize
 import numpy
 import os
 compiler_directives = {"language_level": 3, "embedsignature": True}
+
+import sysconfig
 
 _LIB_OUTPUT_PATH = 'LIB_OUTPUT_PATH'
 _INCLUDE_PATH = 'INCLUDE_PATH'
@@ -41,17 +45,16 @@ if _INCLUDE_PATH in os.environ:
 else:
     print('Unable to build. Please specify an include path with environment variable INCLUDE_PATH')
     sys.exit(1)
-
+print(sysconfig.get_paths()["purelib"] + "/kompile/kompile/interface/native")
 extension = Extension('kompile.interface.native.interface',
-              extra_compile_args=[],
-              extra_link_args=["-Wl,-rpath='$ORIGIN'"],
-              sources=['kompile/interface/native/interface.pyx'],
-              include_dirs=include_list,
-              library_dirs=lib_list,
-            runtime_library_dirs=["."],
-              libraries=['kompile_c_library','kompile-image'],
-              language='c'
-)
+                      extra_compile_args=[],
+                      extra_link_args=["-Wl,-rpath=$ORIGIN", "-Wl,--enable-new-dtags"],
+                      sources=['kompile/interface/native/interface.pyx'],
+                      include_dirs=include_list,
+                      library_dirs=lib_list,
+                      libraries=['kompile_c_library','kompile-image'],
+                      language='c'
+                      )
 
 ext_modules = cythonize(extension, compiler_directives=compiler_directives)
 
@@ -74,9 +77,9 @@ if 'LIB_OUTPUT_PATH' in os.environ:
           ext_modules=ext_modules)
 else:
     setup(name='kompile',
-      version='0.0.1',
-      author='Adam Gibson',
-      author_email='adam@konduit.ai',
-      packages=packages,
-      setup_requires=['wheel'],
-      ext_modules=ext_modules)
+          version='0.0.1',
+          author='Adam Gibson',
+          author_email='adam@konduit.ai',
+          packages=packages,
+          setup_requires=['wheel'],
+          ext_modules=ext_modules)
