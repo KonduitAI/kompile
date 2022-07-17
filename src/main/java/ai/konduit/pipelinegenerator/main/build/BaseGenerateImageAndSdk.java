b4320408b5dcf8e481708cd0728a5a8a48c98c8b
Adam Gibson <adam@konduit.ai>
2022-07-18 08:38:14 +0900

    Clean up generate image scripts and delegator classes to use inheritance

---
+++ src/main/java/ai/konduit/pipelinegenerator/main/build/BaseGenerateImageAndSdk.java
@@ -0,0 +1,359 @@
+/*
+ * Copyright (c) 2022 Konduit K.K.
+ *
+ *     This program and the accompanying materials are made available under the
+ *     terms of the Apache License, Version 2.0 which is available at
+ *     https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *     WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *     License for the specific language governing permissions and limitations
+ *     under the License.
+ *
+ *     SPDX-License-Identifier: Apache-2.0
+ */
+
+package ai.konduit.pipelinegenerator.main.build;
+
+import ai.konduit.pipelinegenerator.main.Info;
+import ai.konduit.serving.data.image.step.ndarray.ImageToNDArrayStep;
+import ai.konduit.serving.models.deeplearning4j.step.DL4JStep;
+import ai.konduit.serving.models.onnx.step.ONNXStep;
+import ai.konduit.serving.models.samediff.step.SameDiffStep;
+import ai.konduit.serving.models.tensorflow.step.TensorFlowStep;
+import ai.konduit.serving.models.tvm.step.TVMStep;
+import ai.konduit.serving.pipeline.impl.pipeline.SequencePipeline;
+import ai.konduit.serving.python.PythonStep;
+import ai.konduit.serving.vertx.config.InferenceConfiguration;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.nd4j.common.io.ClassPathResource;
+import org.zeroturnaround.exec.ProcessExecutor;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.concurrent.Callable;
+
+/**
+ * Base class for working with any generate-image-and-sdk.sh
+ * command delegation.
+ */
+public abstract class BaseGenerateImageAndSdk implements Callable<Integer> {
+
+    @CommandLine.Option(names = {"--pipelineFile"},description = "Whether to use a pipeline file or not",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String pipelineFile;
+    @CommandLine.Option(names = {"--imageName"},description = "Name of image output file",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String imageName = "kompile-image";
+    @CommandLine.Option(names = {"--kompilePythonPath"},description = "Path to kompile python sdk",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String kompilePythonPath;
+    @CommandLine.Option(names = {"--kompileCPath"},description = "Path to kompile c library",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String kompileCPath;
+
+    @CommandLine.Option(names = {"--pomGenerateOutputPath"},description = "Output path of the generated pom.xml for compiling native image",
+            required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String pomGenerateOutputPath = "pom2.xml";
+    @CommandLine.Option(names = {"--libOutputPath"},description = "Location of where to put c library after compilation",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String libOutputPath = "./lib";
+    @CommandLine.Option(names = {"--includePath"},description = "Location of include path for compilation/linking",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String includePath = "./include";
+    @CommandLine.Option(names = {"--bundleOutputPath"},description = "Path to output file of complete bundle",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String bundleOutputPath;
+    @CommandLine.Option(names = {"--mavenHome"},description = "The maven home location for compiling native image. Defaults to $HOME/.kompile/mvn - this is with the assumption " +
+            "the user is using kompile install for maintaining kompile dependencies. A user however may override this.",
+            required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String mavenHome = Info.mavenDirectory().getAbsolutePath();
+    @CommandLine.Option(names = {"--buildPlatform"},description = "The platform to build for, usually a javacpp.platform value such as linux-x86_64",
+            required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String buildPlatform;
+    @CommandLine.Option(names = {"--binaryExtension"},description = "The platform to build for, usually a javacpp.platform value such as linux-x86_64",
+            required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String binaryExtension;
+    @CommandLine.Option(names = {"--nd4jBackend"},description = "The nd4j backend to use in the image",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String nd4jBackend = "nd4j-native";
+    @CommandLine.Option(names = {"--nd4jClassifier"},description = "The nd4j classifier to use",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String nd4jClassifier = "linux-x86_64";
+
+    @CommandLine.Option(names = {"--nd4jOperations"},description = "The operations to build with libnd4j. If left empty," +
+            " just builds with all ops. Otherwise builds with all ops. Op list is separated with a ;. " +
+            "These operations are parsed as cmake lists.",scope = CommandLine.ScopeType.INHERIT)
+    protected String nd4jOperations = "";
+    @CommandLine.Option(names = {"--nd4jDataTypes"},
+            description = "The data types to build with libnd4j. If left empty, just builds with all data types." +
+                    " Otherwise builds with all data types. Data type list is separated with a ;. These operations are parsed as cmake lists.",scope = CommandLine.ScopeType.INHERIT)
+    protected String nd4jDataTypes = "";
+
+
+    @CommandLine.Option(names = {"--nd4jHelper"},description = "The nd4j classifier to use",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String nd4jHelper;
+
+
+    @CommandLine.Option(names = {"--enableJetsonNano"},description = "Whether to use jetson nano dependencies or not",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected boolean enableJetsonNano = false;
+    @CommandLine.Option(names = {"--buildSharedLibrary"},description = "Whether to build a shared library or not, defaults to true"
+            ,required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected boolean buildSharedLibrary = true;
+    @CommandLine.Option(names = {"--mainClass"},description = "The entry point to use in the image",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected String mainClass;
+    @CommandLine.Option(names = {"--minRamMegs"},description = "The minimum memory usage for the image",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected long minRamMegs = 2000;
+    @CommandLine.Option(names = {"--maxRamMegs"},description = "The maximum memory usage for the image",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected long maxRamMegs = 2000;
+    @CommandLine.Option(names = {"--noGc"},description = "Whether to use gc in the image or not",required = false,scope = CommandLine.ScopeType.INHERIT)
+    protected boolean noGc = false;
+
+    @CommandLine.Option(names = "--nativeImageFilesPath",description = "The path to the files for building an image",scope = CommandLine.ScopeType.INHERIT)
+    protected String nativeImageFilesPath;
+
+    @CommandLine.Option(names = "--kompilePrefix",description = "The kompile prefix where the relevant kompile source code is for compilation.",scope = CommandLine.ScopeType.INHERIT)
+    protected String kompilePrefix = "./";
+    @CommandLine.Option(names = "--pythonExecutable",description = "The executable to use with python. " +
+            "Defaults to the python found on the path. Otherwise will use the built in python installed with ./kompile install python",scope = CommandLine.ScopeType.INHERIT)
+    protected String pythonExecutable = "python";
+
+
+    @CommandLine.Option(names = {"--python"},description = "Whether to use python or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean python = false;
+    @CommandLine.Option(names = {"--onnx"},description = "Whether to use onnx or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean onnx = false;
+    @CommandLine.Option(names = {"--tvm"},description = "Whether to use tvm or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean tvm = false;
+    @CommandLine.Option(names = {"--dl4j"},description = "Whether to use dl4j or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean dl4j = false;
+    @CommandLine.Option(names = "--samediff",description = "Whether to use samediff or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean samediff = false;
+    @CommandLine.Option(names = "--nd4j",description = "Whether to use nd4j or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean nd4j = false;
+    @CommandLine.Option(names = "--server",description = "Whether to the expected file is a server configuration or not. Defaults to false.",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean server = false;
+
+    @CommandLine.Option(names = "--tensorflow",description = "Whether to use tensorflow or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean tensorflow = false;
+    @CommandLine.Option(names = "--nd4j-tensorflow",description = "Whether to use nd4j-tensorflow or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean nd4jTensorflow = false;
+    @CommandLine.Option(names = "--image",description = "Whether to use image pre processing or not or not",scope = CommandLine.ScopeType.INHERIT)
+    protected boolean image = false;
+
+
+    @CommandLine.Option(names = {"--dl4jBranchName"},description = "The branch to clone for deeplearning4j: defaults to master",scope = CommandLine.ScopeType.INHERIT)
+    protected String dl4jBranchName = "master";
+    @CommandLine.Option(names = {"--konduitServingBranchName"},description = "The branch to clone konduit-serving: defaults to master",scope = CommandLine.ScopeType.INHERIT)
+    protected String konduitServingBranchName = "master";
+
+
+
+    protected boolean anySpecifiedPipelines() {
+        return tensorflow ||
+                tvm ||
+                nd4jTensorflow ||
+                python ||
+                dl4j ||
+                onnx ||
+                samediff ||
+                image;
+    }
+
+    protected File generatePipelineBasedOnSpec() throws IOException {
+        File newPipeline = new File(System.getProperty("java.io.tmpdir"),"temp-pipeline-" + UUID.randomUUID() + ".json");
+        newPipeline.deleteOnExit();
+        SequencePipeline.Builder pipeline = SequencePipeline.builder();
+        if(tvm)
+            pipeline.add(new TVMStep());
+        if(dl4j)
+            pipeline.add(new DL4JStep());
+        if(tensorflow)
+            pipeline.add(new TensorFlowStep());
+        if(python)
+            pipeline.add(new PythonStep());
+        if(samediff)
+            pipeline.add(new SameDiffStep());
+        if(onnx)
+            pipeline.add(new ONNXStep());
+        if(image)
+            pipeline.add(new ImageToNDArrayStep());
+
+
+        SequencePipeline build = pipeline.build();
+        //ensure that it's a server for consistency with isServer flag being true in configuration
+        InferenceConfiguration inferenceConfiguration = new InferenceConfiguration()
+                .pipeline(build);
+
+        FileUtils.write(newPipeline,inferenceConfiguration.toJson(), Charset.defaultCharset());
+        return newPipeline;
+    }
+
+    protected void checkExists(File dir,String module) {
+        if(!dir.exists()) {
+            System.err.println(String.format("Unable to generate image. Please run kompile install %s before continuing. You can also run kompile install all to install all relevant dependencies. Note that other dependencies may need to be installed such as your nd4j backend specific compiler such as gcc, nvcc, or ncc. ",module));
+            System.exit(1);
+        }
+    }
+
+
+    protected void checkInstall() {
+        checkExists(Info.graalvmDirectory(),"graalvm");
+        checkExists(Info.mavenDirectory(),"maven");
+        checkExists(Info.pythonDirectory(),"python");
+
+    }
+
+
+    protected void addCommand(String commandValue,String scriptName,List<String> commandList) {
+        if(commandValue != null && !commandValue.isEmpty()) {
+            commandList.add(scriptName);
+            commandList.add(commandValue);
+        }
+    }
+
+    protected void addCommands(List<String> command) {
+        addCommand(nd4jHelper,"--nd4j-helper",command);
+        addCommand(nd4jDataTypes,"--nd4j-datatypes",command);
+        addCommand(nd4jOperations,"--nd4j-operations",command);
+        addCommand(dl4jBranchName,"--dl4j-branch",command);
+        addCommand(konduitServingBranchName,"--konduit-serving-branch",command);
+        addCommand(pipelineFile,"--pipeline-file",command);
+        addCommand(imageName,"--image-name",command);
+        addCommand(nativeImageFilesPath,"--native-image-file-path",command);
+        addCommand(pomGenerateOutputPath,"--pom-path",command);
+        addCommand(libOutputPath,"--lib-path",command);
+        addCommand(mavenHome,"--maven-home",command);
+        addCommand(kompilePrefix,"--kompile-prefix",command);
+        addCommand(nd4jBackend,"--nd4j-backend",command);
+        addCommand(nd4jClassifier,"--nd4j-classifier",command);
+
+        command.add("--server");
+        command.add(String.valueOf(true));
+        command.add("--enable-jetson-nano");
+        command.add(String.valueOf(enableJetsonNano));
+        command.add("--build-shared");
+        command.add(String.valueOf(buildSharedLibrary));
+
+
+        addCommand(mainClass,"--main-class",command);
+
+
+        command.add("--min-ram");
+        command.add(String.valueOf(minRamMegs));
+        command.add("--max-ram");
+        command.add(String.valueOf(maxRamMegs));
+        command.add("--no-garbage-collection");
+        command.add(String.valueOf(noGc));
+        System.out.println("Running generate image command: " + command);
+    }
+
+    protected void extractResources(File kompileResources) throws IOException {
+        for(String s : new String[] {"numpy_struct.h","konduit-serving.h"}) {
+            ClassPathResource classPathResource = new ClassPathResource(s);
+            try(InputStream is = classPathResource.getInputStream()) {
+                String headerContent = IOUtils.toString(is);
+                File tempFile = new File(kompileResources,s);
+                tempFile.createNewFile();
+                tempFile.deleteOnExit();
+                FileUtils.write(tempFile,headerContent,false);
+            }
+        }
+    }
+
+
+    protected File extractScript(InputStream is) throws IOException {
+        String scriptContent = IOUtils.toString(is);
+        File tempFile = new File(kompilePrefix,"generate-image-and-sdk.sh");
+        tempFile.createNewFile();
+        tempFile.deleteOnExit();
+        FileUtils.write(tempFile,scriptContent,false);
+        tempFile.setExecutable(true);
+        return tempFile;
+    }
+
+    protected void setDefaultFlagsBasedOnPipeline() {
+        if(anySpecifiedPipelines() && pipelineFile != null) {
+            System.err.println("Please only specify a pipeline file or a set of target components.");
+            System.exit(1);
+        } else if(!anySpecifiedPipelines() && pipelineFile == null) {
+            tvm = true;
+            tensorflow = true;
+            nd4jTensorflow = true;
+            dl4j = true;
+            samediff = true;
+            nd4j = true;
+            python = true;
+            onnx = true;
+            System.out.println("No pipeline file or specific components specified. Building all.");
+        }
+    }
+
+
+    protected void setPipelineFile() throws IOException {
+        if(anySpecifiedPipelines()) {
+            File newPipeline = generatePipelineBasedOnSpec();
+            pipelineFile = newPipeline.getAbsolutePath();
+        }
+    }
+
+    protected Map<String,String> createEnv() {
+        Map<String,String> env = new HashMap<>();
+        //setup graalvm and java home for maven
+        System.out.println("Setting graalvm and java home to " + Info.graalvmDirectory().getAbsolutePath());
+        env.put("GRAALVM_HOME",Info.graalvmDirectory().getAbsolutePath());
+        env.put("JAVA_HOME",Info.graalvmDirectory().getAbsolutePath());
+        return env;
+    }
+
+    protected File createResourceDirectory() {
+        //unpack resources needed for inclusion and linking of files for the generate images script
+        File kompileResources = new File(kompilePrefix,"src/main/resources");
+        if(!kompileResources.exists()) {
+            kompileResources.mkdirs();
+        }
+        return kompileResources;
+    }
+
+
+
+    public abstract void setCustomDefaults();
+    public abstract void doCustomCommands(List<String> commands);
+
+    @Override
+    public Integer call() throws Exception {
+        checkExists(Info.graalvmDirectory(), "graalvm");
+        checkExists(Info.mavenDirectory(), "maven");
+        checkExists(Info.pythonDirectory(), "python");
+
+
+        //unpack resources needed for inclusion and linking of files for the generate images script
+        File kompileResources = createResourceDirectory();
+
+        extractResources(kompileResources);
+
+        ClassPathResource classPathResource = new ClassPathResource("generate-image-and-sdk.sh");
+        try (InputStream is = classPathResource.getInputStream()) {
+            File tempFile = extractScript(is);
+            Map<String, String> env = createEnv();
+            System.out.println("Setting graalvm and java home to " + Info.graalvmDirectory().getAbsolutePath());
+
+            List<String> command = new ArrayList<>();
+            command.add(tempFile.getAbsolutePath());
+            setDefaultFlagsBasedOnPipeline();
+
+            setPipelineFile();
+            //set just in case we need to customize flags
+            setCustomDefaults();
+            //add all command values
+            addCommands(command);
+            //add other values specific to command assumptions
+            doCustomCommands(command);
+            System.out.println("Running generate image command: " + command);
+
+
+            return new ProcessExecutor().environment(env)
+                    .command(command)
+                    .readOutput(true)
+                    .redirectOutput(System.out)
+                    .start().getFuture().get().getExitValue();
+        }
+    }
+}
